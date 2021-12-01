28d114aa571f392494bd16997ed6271b791266a1
Matt Perry <muffinman616@gmail.com>
2021-12-01 14:30:47 -0500

    Day 2 skeleton

---
+++ day2/day2.ts
@@ -0,0 +1,9 @@
+import { readInputLines } from '../shared/utils';
+
+export const part1 = (data: any) => data;
+
+(async () => {
+    const input = await readInputLines('day2');
+
+    console.log(part1(input));
+})();
