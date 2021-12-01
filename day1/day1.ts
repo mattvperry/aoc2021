be69f588082c85546a82327489c34409992c76a8
Matt Perry <muffinman616@gmail.com>
2021-12-01 14:23:09 -0500

    Clean up

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,11 +1,11 @@
-import { readInputLines, zipWith, countBy } from '../shared/utils';
+import { readInputLines, zip, countBy } from '../shared/utils';
 
 const day1 = (data: number[], space: number): number =>
-    countBy(zipWith(data, data.slice(space), (a, b) => a < b), x => x);
+    countBy(zip(data, data.slice(space)), ([a, b]) => a < b);
 
 (async () => {
     const input = await readInputLines('day1');
-    const set = input.map(l => parseInt(l, 10));
+    const set = input.map((l) => parseInt(l, 10));
 
     console.log(day1(set, 1));
     console.log(day1(set, 3));
--- shared/utils.ts
+++ shared/utils.ts
@@ -3,10 +3,16 @@ import path from 'path';
 
 export type Day = `day${number}`;
 
-export type FromEntries<T> = T extends readonly [PropertyKey, infer V] ? { [X in T[0]]?: V } : never;
-export type Entries<T> = T extends { [K in keyof T]: infer V } ? readonly [keyof T, V] : never;
+export type FromEntries<T> = T extends readonly [PropertyKey, infer V]
+    ? { [X in T[0]]?: V }
+    : never;
+export type Entries<T> = T extends { [K in keyof T]: infer V }
+    ? readonly [keyof T, V]
+    : never;
 
-export async function* streamInputLinesAsync<T extends Day>(day: T): AsyncIterableIterator<string> {
+export async function* streamInputLinesAsync<T extends Day>(
+    day: T,
+): AsyncIterableIterator<string> {
     let current = '';
     for await (const x of fs.createReadStream(path.join(day, 'input.txt'))) {
         current = current + x;
@@ -16,14 +22,16 @@ export async function* streamInputLinesAsync<T extends Day>(day: T): AsyncIterab
 
         const lines = current.split('\n');
         const [before, after] = splitAt(lines, lines.length - 1);
-        yield* before.map(l => l.trim());
-        [current] = after
+        yield* before.map((l) => l.trim());
+        [current] = after;
     }
 
     yield current.trim();
 }
 
-export const arrayFromAsyncGenerator = <T>(gen: AsyncIterableIterator<T>): Promise<T[]> =>
+export const arrayFromAsyncGenerator = <T>(
+    gen: AsyncIterableIterator<T>,
+): Promise<T[]> =>
     reduceAsync(gen, [] as T[], async (acc, curr) => [...acc, curr]);
 
 export function* zipper<T>(data: Iterable<T>): IterableIterator<[T[], T, T[]]> {
@@ -46,29 +54,40 @@ export function* map<T, U>(data: Iterable<T>, fn: (curr: T) => U): Iterable<U> {
     }
 }
 
-export async function* mapAsync<T, U>(data: AsyncIterable<T>, fn: (curr: T) => Promise<U>): AsyncIterableIterator<U> {
+export async function* mapAsync<T, U>(
+    data: AsyncIterable<T>,
+    fn: (curr: T) => Promise<U>,
+): AsyncIterableIterator<U> {
     for await (const x of data) {
         yield await fn(x);
     }
 }
 
-export const reduce = <T, U>(data: Iterable<T>, seed: U, fn: (acc: U, curr: T) => U): U => {
+export const reduce = <T, U>(
+    data: Iterable<T>,
+    seed: U,
+    fn: (acc: U, curr: T) => U,
+): U => {
     let acc = seed;
     for (const x of data) {
         acc = fn(acc, x);
     }
 
     return acc;
-}
+};
 
-export const reduceAsync = async <T, U>(data: AsyncIterable<T>, seed: U, fn: (acc: U, curr: T) => Promise<U>): Promise<U> => {
+export const reduceAsync = async <T, U>(
+    data: AsyncIterable<T>,
+    seed: U,
+    fn: (acc: U, curr: T) => Promise<U>,
+): Promise<U> => {
     let acc = seed;
     for await (const x of data) {
         acc = await fn(acc, x);
     }
 
     return acc;
-}
+};
 
 export const readInputLines = <T extends Day>(day: T) =>
     arrayFromAsyncGenerator(streamInputLinesAsync(day));
@@ -82,8 +101,8 @@ export const charFrequency = (input: string): { [k: string]: number } => {
 
 export const memoize = <P extends any[], R, K extends PropertyKey>(
     fn: (...args: P) => R,
-    key: (...args: P) => K
-): (...args: P) => R => {
+    key: (...args: P) => K,
+): ((...args: P) => R) => {
     const memo = {} as Record<K, R>;
     return (...args: P) => {
         const k = key(...args);
@@ -107,18 +126,26 @@ export const splitOn = <T>(xs: T[], on: T): [T[], T[]] => {
     return [xs.slice(0, idx), xs.slice(idx + 1, xs.length)];
 };
 
-type Sliceable = { slice: (start?: number, end?: number) => any, length: number };
-export const splitAt = <T extends Sliceable>(xs: T, i: number): [ReturnType<T['slice']>, ReturnType<T['slice']>] =>
-    [xs.slice(0, i), xs.slice(i, xs.length)];
+type Sliceable = {
+    slice: (start?: number, end?: number) => any;
+    length: number;
+};
+export const splitAt = <T extends Sliceable>(
+    xs: T,
+    i: number,
+): [ReturnType<T['slice']>, ReturnType<T['slice']>] => [
+    xs.slice(0, i),
+    xs.slice(i, xs.length),
+];
 
-export const isDefined = <T>(x: T | undefined): x is T =>
-    x !== undefined;
+export const isDefined = <T>(x: T | undefined): x is T => x !== undefined;
 
 export const countBy = <T>(data: Iterable<T>, fn: (x: T) => boolean): number =>
     reduce(data, 0, (acc, curr) => acc + (fn(curr) ? 1 : 0));
 
-export const fromEntries = <T extends readonly [PropertyKey, any]>(entries: Iterable<T>): FromEntries<T> =>
-    Object.fromEntries(entries) as FromEntries<T>;
+export const fromEntries = <T extends readonly [PropertyKey, any]>(
+    entries: Iterable<T>,
+): FromEntries<T> => Object.fromEntries(entries) as FromEntries<T>;
 
 export const zipWith = <T, U>(xs: T[], ys: T[], fn: (a: T, b: T) => U): U[] => {
     if (xs.length === 0 || ys.length === 0) {
@@ -130,6 +157,9 @@ export const zipWith = <T, U>(xs: T[], ys: T[], fn: (a: T, b: T) => U): U[] => {
     return [fn(x, y), ...zipWith(xr, yr, fn)];
 };
 
+export const zip = <T>(xs: T[], ys: T[]): [T, T][] =>
+    zipWith(xs, ys, (x, y) => [x, y]);
+
 export const repeatFn = <T>(x: T, times: number, fn: (x: T) => T): T => {
     for (let i = 0; i < times; ++i) {
         x = fn(x);
@@ -139,9 +169,9 @@ export const repeatFn = <T>(x: T, times: number, fn: (x: T) => T): T => {
 };
 
 export const intersect = <T>(xs: Set<T>, ys: Set<T>): Set<T> =>
-    new Set([...xs].filter(x => ys.has(x)));
+    new Set([...xs].filter((x) => ys.has(x)));
 
 export const difference = <T>(xs: Set<T>, ys: Set<T>): Set<T> =>
-    new Set([...xs].filter(x => !ys.has(x)));
+    new Set([...xs].filter((x) => !ys.has(x)));
 
 export const mod = (n: number, m: number): number => ((n % m) + m) % m;
