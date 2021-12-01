b8e91e6fbfda0cf11fb033a083ff904f9aa82c97
Matt Perry <forloop616@gmail.com>
2021-12-01 00:27:49 -0500

    Day 1

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,12 +1,12 @@
-import { readInputLines } from '../shared/utils';
+import { readInputLines, zipWith, countBy } from '../shared/utils';
 
-const part1 = (data: any): number => 0;
-
-// const part2 = (data: Set<number>): number => 0;
+const day1 = (data: number[], space: number): number =>
+    countBy(zipWith(data, data.slice(space), (a, b) => a < b), x => x);
 
 (async () => {
     const input = await readInputLines('day1');
+    const set = input.map(l => parseInt(l, 10));
 
-    console.log(part1(input));
-    // console.log(part2(input));
+    console.log(day1(set, 1));
+    console.log(day1(set, 3));
 })();
