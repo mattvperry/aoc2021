d77fb15b8991b969161ab83bf7e4d57598697650
Matt Perry <forloop616@gmail.com>
2021-12-02 23:45:39 -0500

    Day 3 skele

---
+++ day3/day3.ts
@@ -0,0 +1,11 @@
+import { readInputLines } from '../shared/utils';
+
+const part1 = (data: string[]) => {
+    return data;
+};
+
+(async () => {
+    const input = await readInputLines('day3');
+
+    console.log(part1(input));
+})();
