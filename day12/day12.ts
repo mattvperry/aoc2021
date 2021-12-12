0ae8534624096067b8ade70bdb8b525ba8b26235
Matt Perry <forloop616@gmail.com>
2021-12-12 17:06:24 -0500

    Day 12 fixes

--- day12/day12.ts
+++ day12/day12.ts
@@ -6,8 +6,13 @@ const parse = (lines: string[]): Graph => {
     const graph: Graph = {};
     const paths = lines.map(l => l.split('-'));
     for (const [a, b] of paths) {
-        graph[a] = [b, ...(graph[a] ?? [])];
-        graph[b] = [a, ...(graph[b] ?? [])];
+        if (b !== 'start') {
+            graph[a] = [b, ...(graph[a] ?? [])];
+        }
+
+        if (a !== 'start') {
+            graph[b] = [a, ...(graph[b] ?? [])];
+        }
     }
 
     return graph;
@@ -23,10 +28,14 @@ const search = (
         return 1;
     }
 
+    // A problem is any small cave that we've visited more
+    // than once
     const problems = entries(seen).filter(
         ([n, x]) => n === n.toLocaleLowerCase() && x > 1,
     );
 
+    // If we encounter any problems, decide if we need to
+    // prune this path based on the pruning predicate fn
     if (problems.length > 0 && prune(problems)) {
         return 0;
     }
@@ -41,16 +50,16 @@ const search = (
 // prune the branch.
 const part1 = () => true;
 
-// For part2, if we have more than one problem, or
-// we are visiting start again, or if we have visited
-// this more than twice then we prune the branch.
-const part2 = ([[n, x], ...rest]: [string, number][]): boolean =>
-    rest.length !== 0 || n === 'start' || x > 2;
+// For part2, if we have more than one problem,
+// or if we have visited the small cave more than
+// twice then we prune the branch.
+const part2 = ([[_, x], ...rest]: [string, number][]): boolean =>
+    rest.length !== 0 || x > 2;
 
 const day12 = (
     graph: Graph,
     prune: (problems: [string, number][]) => boolean,
-): number => search(graph, 'start', { start: 1 }, prune);
+): number => search(graph, 'start', {}, prune);
 
 (async () => {
     const input = await readInputLines('day12');
